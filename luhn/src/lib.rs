ed247a56f10c58b3c905dc5a458ab0d42b753297
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-01-05 15:01:59 +0300

    idiomatic solution

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,34 +1,19 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    if code.len() < 2 {
-        return false;
-    }
-
-    let mut sum = 0;
-    let mut char_counter = 0;
-    let rev_chars = code.chars().rev();
-
-    for v in rev_chars {
-        if v.is_whitespace() && char_counter % 2 == 0 {
-            char_counter = 0;
-        } else if v.is_numeric() {
-            let mut parsed_v = v.to_string().parse::<i32>().unwrap();
-            if char_counter % 2 != 0 {
-                parsed_v *= 2;
-                if parsed_v > 9 {
-                    parsed_v -= 9
-                }
+    code.chars()
+        .rev()
+        .filter(|c| !c.is_whitespace())
+        .try_fold((0, 0), |(sum, count), curr| {
+            curr.to_digit(10)
+                .map(|num| if count % 2 != 0 { num * 2 } else { num })
+                .map(|num| if num > 9 { num - 9 } else { num })
+                .map(|num| (num + sum, count + 1))
+        })
+        .map_or(false, |(sum, count)| {
+            if sum % 10 == 0 && count > 1 {
+                true
+            } else {
+                false
             }
-
-            sum += parsed_v;
-            char_counter += 1
-        } else if v.is_ascii() && !v.is_whitespace() {
-            if sum % 10 == 0 {
-                sum += 1;
-            }
-            break;
-        }
-    }
-
-    (sum == 0 && code.trim().len() > 1) || (sum > 0 && sum % 10 == 0)
+        })
 }
