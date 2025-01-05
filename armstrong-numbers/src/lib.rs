83cc66f5aa9ba3235ae3c557572e64bafc4cdbd4
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-01-05 16:10:29 +0300

    armstrong number

---
+++ armstrong-numbers/src/lib.rs
@@ -0,0 +1,10 @@
+pub fn is_armstrong_number(num: u32) -> bool {
+    let num_str = num.to_string();
+    let len = num_str.len() as u32;
+
+    num_str
+        .chars()
+        .map(|num| num.to_digit(10).unwrap().pow(len))
+        .sum::<u32>()
+        == num
+}
