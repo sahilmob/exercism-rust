22bb4ac42d7353f73d06b9afeccaaf1208ae1df3
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-01-06 17:08:14 +0300

    remove unessecarry if

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -3,12 +3,11 @@ pub fn factors(n: u64) -> Vec<u64> {
     let mut i = 2;
     let mut result: Vec<u64> = Vec::new();
     while i <= n {
-        if n % i == 0 {
-            while n % i == 0 {
-                result.push(i);
-                n /= i
-            }
+        while n % i == 0 {
+            result.push(i);
+            n /= i
         }
+
         i += 1
     }
 
