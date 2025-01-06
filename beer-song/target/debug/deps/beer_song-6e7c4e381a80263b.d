c1e604ddb277fee21ed130e05b1b8ef527a2a26c
Sahil H. Mubaideen <Sahil.mubaideen@network.global>
2025-01-06 09:57:28 +0300

    beer song

---
+++ beer-song/target/debug/deps/beer_song-6e7c4e381a80263b.d
@@ -0,0 +1,5 @@
+/Users/sahilmu/Desktop/Exercism/rust/beer-song/target/debug/deps/beer_song-6e7c4e381a80263b: tests/beer-song.rs
+
+/Users/sahilmu/Desktop/Exercism/rust/beer-song/target/debug/deps/beer_song-6e7c4e381a80263b.d: tests/beer-song.rs
+
+tests/beer-song.rs:
---
+++ beer-song/target/debug/deps/beer_song-d0593cfa9fe35e7f.d
@@ -0,0 +1,5 @@
+/Users/sahilmu/Desktop/Exercism/rust/beer-song/target/debug/deps/libbeer_song-d0593cfa9fe35e7f.rmeta: tests/beer-song.rs
+
+/Users/sahilmu/Desktop/Exercism/rust/beer-song/target/debug/deps/beer_song-d0593cfa9fe35e7f.d: tests/beer-song.rs
+
+tests/beer-song.rs:
