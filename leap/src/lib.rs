c8969a25b213f03905ac6aa5b6e48d25871e6aec
Sahil H. Mubaideen <Sahil.mubaideen@network.global>
2025-01-06 11:42:48 +0300

    leap year

---
+++ leap/src/lib.rs
@@ -0,0 +1,22 @@
+pub fn is_leap_year(year: u64) -> bool {
+    match year % 4 {
+        0 => {
+            if year % 100 == 0 {
+                if year % 400 == 0 {
+                    true
+                } else {
+                    false
+                }
+            } else {
+                true
+            }
+        }
+        _ => {
+            if year % 100 == 0 && year % 400 == 0 {
+                true
+            } else {
+                false
+            }
+        }
+    }
+}
