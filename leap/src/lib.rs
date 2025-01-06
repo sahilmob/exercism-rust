615baf993b856c97fe10b474dd55924b2d68d3ea
Sahil H. Mubaideen <Sahil.mubaideen@network.global>
2025-01-06 11:44:24 +0300

    simplify code

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -2,21 +2,11 @@ pub fn is_leap_year(year: u64) -> bool {
     match year % 4 {
         0 => {
             if year % 100 == 0 {
-                if year % 400 == 0 {
-                    true
-                } else {
-                    false
-                }
+                year % 400 == 0
             } else {
                 true
             }
         }
-        _ => {
-            if year % 100 == 0 && year % 400 == 0 {
-                true
-            } else {
-                false
-            }
-        }
+        _ => year % 100 == 0 && year % 400 == 0,
     }
 }
