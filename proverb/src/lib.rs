18046a59dd0f3ddc7c42dd7056447ce6f8e73124
Sahil H. Mubaideen <Sahil.mubaideen@network.global>
2025-01-07 13:12:49 +0300

    idiomatic solution

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,21 +1,11 @@
+use std::iter::once;
 pub fn build_proverb(list: &[&str]) -> String {
-    let mut result = String::from("");
-
-    if list.len() == 0 {
-        return result;
+    match list.first() {
+        None => String::new(),
+        Some(word) => list
+            .windows(2)
+            .map(|w| format!("For want of a {} the {} was lost.\n", w[0], w[1]))
+            .chain(once(format!("And all for the want of a {}.", word)))
+            .collect(),
     }
-
-    let final_str = format!("And all for the want of a {}.", list[0]);
-
-    (0..list.len()).for_each(|i| {
-        if i < list.len() - 1 {
-            result.push_str(
-                format!("For want of a {} the {} was lost.\n", list[i], list[i + 1]).as_str(),
-            );
-        }
-    });
-
-    result.push_str(&final_str);
-
-    result
 }
