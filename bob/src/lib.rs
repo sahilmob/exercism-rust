3b2124146930c1624eeb9f8a0f77dc780c85fc07
Sahil H. Mubaideen <Sahil.mubaideen@network.global>
2025-01-08 12:13:02 +0300

    idiomatic solution

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,19 +1,13 @@
+fn is_yelling(message: &str) -> bool {
+    let have_letters: bool = message.chars().filter(|x| x.is_alphabetic()).count() > 0;
+    message.to_uppercase() == message && have_letters
+}
 pub fn reply(message: &str) -> &str {
-    if message.trim() == "" {
-        return "Fine. Be that way!";
+    match message.trim() {
+        m if m.trim().len() == 0 => "Fine. Be that way!",
+        m if m.ends_with("?") && is_yelling(m) => "Calm down, I know what I'm doing!",
+        m if m.ends_with("?") => "Sure.",
+        m if is_yelling(m) => "Whoa, chill out!",
+        _ => "Whatever.",
     }
-
-    let bytes = message.trim().as_bytes();
-
-    if bytes.last().unwrap() == &b'?' {
-        if bytes[..bytes.len() - 1].iter().all(|b| *b < 97) && bytes.iter().any(|b| *b > 64) {
-            return "Calm down, I know what I'm doing!";
-        } else {
-            return "Sure.";
-        }
-    } else if bytes.iter().all(|b| *b < 97) && bytes.iter().any(|b| *b > 64) {
-        return "Whoa, chill out!";
-    }
-
-    "Whatever."
 }
