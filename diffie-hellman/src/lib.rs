add2ccb653c6d10c7e40426f9f8b100d4a51145d
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-01-10 16:07:46 +0300

    diffie hellman

--- diffie-hellman/src/lib.rs
+++ diffie-hellman/src/lib.rs
@@ -7,9 +7,10 @@ pub fn private_key(p: u64) -> u64 {
     let _ = (2..=rng.gen_range(2..p)).filter(|x| {
         if !primes.iter().any(|n| x % n == 0) {
             primes.push(*x);
-            return true;
+            true
+        } else {
+            false
         }
-        return false;
     });
 
     if primes.last().is_some() {
@@ -20,25 +21,21 @@ pub fn private_key(p: u64) -> u64 {
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    let mut c = 1;
-    let mut e_prime = 0;
+    let mut e = a;
+    let mut b = g;
+    let mut result = 1;
 
-    while e_prime < a {
-        e_prime += 1;
-        c = (c * g) % p
+    while e > 0 {
+        if e % 2 == 1 {
+            result = (result * b) % p;
+        }
+        b = (b * b) % p;
+        e /= 2;
     }
 
-    c
+    result
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    let mut c = 1;
-    let mut e_prime = 0;
-
-    while e_prime < a {
-        e_prime += 1;
-        c = (c * b_pub) % p
-    }
-
-    c
+    public_key(p, b_pub, a)
 }
