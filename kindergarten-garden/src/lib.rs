d7a5f6f0221130c3559504e4ffca32db058c5181
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-01-11 11:03:40 +0300

    idiomatic solution

--- kindergarten-garden/src/lib.rs
+++ kindergarten-garden/src/lib.rs
@@ -4,25 +4,17 @@ const STUDENTS: [&str; 12] = [
 ];
 
 pub fn plants(diagram: &str, student: &str) -> Vec<&'static str> {
-    let student_idx = STUDENTS.iter().position(|n| *n == student).unwrap();
-
-    let mut result: Vec<&str> = Vec::new();
-    let start_idx = 2 * student_idx;
-    let ent_idx = start_idx + 2;
+    let cup_idx = STUDENTS.iter().position(|n| *n == student).unwrap() * 2;
 
     diagram
-        .split("\n")
-        .collect::<Vec<_>>()
-        .iter()
-        .for_each(|r| {
-            r[start_idx..ent_idx].chars().for_each(|c| match c {
-                'C' => result.push("clover"),
-                'G' => result.push("grass"),
-                'R' => result.push("radishes"),
-                'V' => result.push("violets"),
-                _ => (),
-            });
-        });
-
-    result
+        .lines()
+        .flat_map(|l| {
+            l[cup_idx..=cup_idx + 1].chars().map(|c| match c {
+                'C' => "clover",
+                'G' => "grass",
+                'R' => "radishes",
+                _ => "violets",
+            })
+        })
+        .collect()
 }
