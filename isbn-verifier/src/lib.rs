91c7fbfff419d1d89ffa23c96828109fad996cdd
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-01-13 20:27:38 +0300

    use implicit return

--- isbn-verifier/src/lib.rs
+++ isbn-verifier/src/lib.rs
@@ -5,7 +5,7 @@ pub fn is_valid_isbn(isbn: &str) -> bool {
             '-' => (c, s, v),
             'X' if c == 9 => (c + 1, (s + 10) % 11, v),
             '0'..='9' => (c + 1, (s + curr.to_digit(10).unwrap() * (10 - c)) % 11, v),
-            _ => return (c, s, false),
+            _ => (c, s, false),
         })
         == (10, 0, true)
 }
