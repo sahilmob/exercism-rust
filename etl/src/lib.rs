c0e98969aab3bf795c25f4ad7492674b93c44537
Sahil H. Mubaideen <Sahil.mubaideen@network.global>
2025-01-13 15:08:31 +0300

    idiomatic solution

--- etl/src/lib.rs
+++ etl/src/lib.rs
@@ -1,15 +1,11 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    let mut result: BTreeMap<char, i32> = BTreeMap::new();
-
-    h.iter().for_each(|(score, chars)| {
-        let mut chars = chars.clone();
-        chars.sort();
-        chars.iter().for_each(|c| {
-            result.insert(c.to_ascii_lowercase(), *score);
-        });
-    });
-
-    result
+    h.iter()
+        .flat_map(|(score, letters)| {
+            letters
+                .iter()
+                .map(move |letter| (letter.to_ascii_lowercase(), *score))
+        })
+        .collect()
 }
