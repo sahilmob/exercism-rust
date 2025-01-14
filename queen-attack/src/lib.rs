358113dd6059a32d36dc2a8bf7bebb50fad09955
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-01-14 18:25:01 +0300

    queen attack

---
+++ queen-attack/src/lib.rs
@@ -0,0 +1,37 @@
+#[derive(Debug)]
+pub struct ChessPosition {
+    rank: i32,
+    file: i32,
+}
+
+#[derive(Debug)]
+pub struct Queen {
+    position: ChessPosition,
+}
+
+impl ChessPosition {
+    pub fn new(rank: i32, file: i32) -> Option<Self> {
+        match (rank, file) {
+            (0..=7, 0..=7) => Some(Self { rank, file }),
+            _ => None,
+        }
+    }
+}
+
+impl Queen {
+    pub fn new(position: ChessPosition) -> Self {
+        Self { position }
+    }
+
+    pub fn can_attack(&self, other: &Queen) -> bool {
+        if self.position.rank == other.position.rank
+            || self.position.file == other.position.file
+            || (self.position.file - other.position.file).abs()
+                == (self.position.rank - other.position.rank).abs()
+        {
+            return true;
+        }
+
+        false
+    }
+}
