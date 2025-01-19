c3e290d516e38661f3a03a86a37b9c56d064c0e9
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-01-19 18:03:45 +0300

    Simple Cipher

---
+++ simple-cipher/src/lib.rs
@@ -0,0 +1,50 @@
+fn check_lower_case_letters(s: &str) -> bool {
+    for c in s.chars() {
+        if c.is_uppercase() || c.is_numeric() || c.is_uppercase() {
+            return false;
+        }
+    }
+    return true;
+}
+
+pub fn encode(key: &str, s: &str) -> Option<String> {
+    if key.is_empty() || !check_lower_case_letters(key) || !check_lower_case_letters(s) {
+        return None;
+    }
+
+    let mut res = String::new();
+    let mut iter_key = key.chars().cycle();
+
+    for s_char in s.chars() {
+        let key_char = iter_key.next().unwrap();
+        res.push((((s_char as u8 - 97) + (key_char as u8 - 97) + 26) % 26 + 97) as char);
+    }
+
+    Some(res)
+}
+
+pub fn decode(key: &str, s: &str) -> Option<String> {
+    if key.is_empty() || !check_lower_case_letters(key) || !check_lower_case_letters(s) {
+        return None;
+    }
+
+    let mut res = String::new();
+    let mut iter_key = key.chars().cycle();
+
+    for s_char in s.chars() {
+        let key_char = iter_key.next().unwrap();
+        res.push(((((s_char as i8 - 97) - (key_char as i8 - 97) + 26) as u8) % 26 + 97) as char);
+    }
+
+    Some(res)
+}
+
+pub fn encode_random(s: &str) -> (String, String) {
+    let key = (0..200)
+        .map(|_| (rand::random::<u8>() % 26 + 97) as char)
+        .collect::<String>();
+
+    let encrypted = encode(&key[..], s).unwrap();
+
+    (key.to_string(), encrypted)
+}
