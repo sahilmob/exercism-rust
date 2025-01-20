b72cec1fb47af0289cb0d5ca6a70dbb7e56d7ed1
Sahil H. Mubaideen <Sahil.mubaideen@network.global>
2025-01-20 13:02:08 +0300

    idiomatic solution

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,27 +1,13 @@
 #[macro_export]
 macro_rules! hashmap {
-    // Empty case
-    () => {{
-        use crate::HashMap;
-        HashMap::new()
-    }};
-
-    // Single pair with optional trailing comma
-    ($key:literal => $v:expr $(,)?) => {{
-        use crate::HashMap;
-        let mut hm = HashMap::new();
-        hm.insert($key, $v);
-        hm
-    }};
-
-    // Multiple pairs with required comma
-    ($key:literal => $v:expr, $($rest:literal => $rv:expr),+ $(,)?) => {{
-        use crate::HashMap;
-        let mut hm = HashMap::new();
-        hm.insert($key, $v);
-        $(
-            hm.insert($rest, $rv);
-        )+
-        hm
-    }};
+    () => {
+        ::std::collections::HashMap::new()
+    };
+    ( $($key:expr => $value:expr),+ $(,)? ) => {
+        {
+            let mut hm = ::std::collections::HashMap::new();
+            $( hm.insert($key, $value); )*
+            hm
+        }
+    };
 }
